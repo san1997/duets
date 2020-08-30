63e55b73cbbdc8d58c4563a9e984f042f9a06da7
Sandeep Pal <sandeeppal@alphonso.tv>
2020-08-30 14:28:24 +0530

    adding firebase to app

--- App.js
+++ App.js
@@ -2,10 +2,40 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+import * as firebase from 'firebase';
+
+// Optionally import the services that you want to use
+//import "firebase/auth";
+//import "firebase/database";
+//import "firebase/firestore";
+//import "firebase/functions";
+//import "firebase/storage";
+
+// Initialize Firebase
+const firebaseConfig = {
+  apiKey: "AIzaSyDsUQKD5-aYCRYxfe0UoIhsbDZNdPLtvjc",
+  authDomain: "duets-app-a40c0.firebaseapp.com",
+  databaseURL: "https://duets-app-a40c0.firebaseio.com",
+  projectId: "duets-app-a40c0",
+  storageBucket: "duets-app-a40c0.appspot.com",
+};
+
+if (!firebase.apps.length) {
+   firebase.initializeApp(firebaseConfig);
+}
+
+var database = firebase.database();
+
 export default function App() {
+  const temp = 'test1';
+  let ans = 'start';
+  var starCountRef = firebase.database().ref('user');
+  starCountRef.on('value', function(snapshot) {
+    ans = snapshot;
+  });
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>{`App.js is the main${JSON.stringify(ans)} file here!`}</Text>
       <StatusBar style="auto" />
     </View>
   );
