c216e9b928f220be75ca866ce57838a855f86a5f
Sandeep Pal <sandeeppal@alphonso.tv>
2020-09-04 14:57:43 +0530

    adding 3 screens for feed,upload,profile

---
+++ src/screens/homeScreen/style.js
@@ -0,0 +1,8 @@
+import styled from "styled-components/native";
+
+export const HomeScreenStyles = styled.View`
+  flex: 1;
+  background-color: #fff;
+  align-items: center;
+  justify-content: center;
+`;
