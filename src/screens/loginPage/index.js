a82be7c7f348f92c876197a9d99fe6e3504ad95f
Suyash Gupta <suyashshruti@gmail.com>
2020-09-06 03:00:11 +0530

    added splash page and navigation

--- src/screens/loginPage/index.js
+++ src/screens/loginPage/index.js
@@ -1,18 +1,30 @@
-// import React from "react";
-// import styled from "styled-components/native";
+import React from "react";
+import { Button } from "react-native";
+import { useNavigation } from "@react-navigation/native";
 
-// import { HomePageStyles, Title, Logo } from "./style.js";
-// import logoImage from "./logo.png";
+import { LoginPageStyles, Title, Logo } from "./style.js";
+import logoImage from "../../assets/logo.png";
 
-// class HomePage extends React.Component {
-//   render() {
-//     return (
-//       <HomePageStyles>
-//         <Logo source={logoImage}></Logo>
-//         <Title>Welcome to Duet</Title>
-//       </HomePageStyles>
-//     );
-//   }
-// }
+function GoToButton({ screenName }) {
+  const navigation = useNavigation();
+  return (
+    <Button
+      title={`Go to ${screenName}`}
+      onPress={() => navigation.navigate(screenName)}
+    />
+  );
+}
 
-// export default HomePage;
+class LoginPage extends React.Component {
+  render() {
+    return (
+      <LoginPageStyles>
+        <Logo source={logoImage}></Logo>
+        <Title>Login Page</Title>
+        <GoToButton screenName="HomeScreen" />
+      </LoginPageStyles>
+    );
+  }
+}
+
+export default LoginPage;
---
+++ src/screens/loginPage/style.js
@@ -0,0 +1,20 @@
+import styled from "styled-components/native";
+
+export const LoginPageStyles = styled.View`
+  flex: 1;
+  background-color: #fff;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const Title = styled.Text`
+  color: #222;
+  text-align: center;
+  font-size: 20px;
+  margin-top: 30px;
+`;
+
+export const Logo = styled.Image`
+  width: 150px;
+  height: 150px;
+`;
