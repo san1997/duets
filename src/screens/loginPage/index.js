6b1dfcf0f0c233686c4fc6410e9dd7b43efe4923
Suyash Gupta <suyashshruti@gmail.com>
2020-09-06 14:38:09 +0530

    added first login page UI

--- src/screens/loginPage/index.js
+++ src/screens/loginPage/index.js
@@ -1,9 +1,12 @@
 import React from "react";
-import { Button } from "react-native";
+import { Button, Image, Text, View, TextInput, Linking } from "react-native";
 import { useNavigation } from "@react-navigation/native";
 
-import { LoginPageStyles, Title, Logo } from "./style.js";
-import logoImage from "../../assets/logo.png";
+import strings from "../../constConfig/strings";
+import colors from "../../constConfig/colors";
+import images from "../../constConfig/images";
+
+import { LoginPageStyles, loginStyles } from "./style.js";
 
 function GoToButton({ screenName }) {
   const navigation = useNavigation();
@@ -19,9 +22,42 @@ class LoginPage extends React.Component {
   render() {
     return (
       <LoginPageStyles>
-        <Logo source={logoImage}></Logo>
-        <Title>Login Page</Title>
-        <GoToButton screenName="HomeScreen" />
+        <View style={loginStyles.logoContainer}>
+          <Image style={loginStyles.loginLogo} source={images.logoImage} />
+          <Text style={loginStyles.loginText}> {strings.loginMessage}</Text>
+        </View>
+        <View style={loginStyles.loginHeadingContainer}>
+          <Text style={loginStyles.loginHeading}> {strings.loginHeading}</Text>
+        </View>
+        <View style={loginStyles.loginFormContainer}>
+          <TextInput
+            placeholder={"Enter Mobile/ Email Id"}
+            placeholderTextColor={colors.textLightColor}
+            style={loginStyles.loginForminput}
+          />
+          <TextInput
+            placeholder={"Continue"}
+            placeholderTextColor={colors.textLightColor}
+            style={loginStyles.loginForminput}
+          />
+        </View>
+        <View style={loginStyles.forgotPassContainer}>
+          <Text
+            style={loginStyles.forgotPassHeading}
+            onPress={() => Linking.openURL("https://google.com")}
+          >
+            {strings.forgotPass}
+          </Text>
+        </View>
+        <View style={loginStyles.orDivContainer}>
+          <Text style={loginStyles.orDivText}>{strings.orHeading}</Text>
+        </View>
+        <View style={loginStyles.signupHeadingContainer}>
+          <Text style={loginStyles.signupHeading}>{strings.signupHeading}</Text>
+        </View>
+        <View style={loginStyles.goToHomeButton}>
+          <GoToButton screenName="HomeScreen" />
+        </View>
       </LoginPageStyles>
     );
   }
--- src/screens/loginPage/style.js
+++ src/screens/loginPage/style.js
@@ -1,4 +1,8 @@
 import styled from "styled-components/native";
+import { StyleSheet } from "react-native";
+
+import colors from "../../constConfig/colors";
+import fonts from "../../constConfig/fonts";
 
 export const LoginPageStyles = styled.View`
   flex: 1;
@@ -18,3 +22,91 @@ export const Logo = styled.Image`
   width: 150px;
   height: 150px;
 `;
+
+export const loginStyles = StyleSheet.create({
+  logoContainer: {
+    position: "absolute",
+    width: "70%",
+    top: "10%",
+    alignItems: "center",
+  },
+  loginLogo: {
+    width: 80,
+    height: 80,
+  },
+  loginText: {
+    top: "12%",
+    textAlign: "center",
+    fontFamily: fonts.regular_text,
+    color: colors.grey,
+  },
+  loginHeadingContainer: {
+    position: "absolute",
+    top: "30%",
+    backgroundColor: colors.backgroundLight,
+    width: "100%",
+    height: "5%",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  loginHeading: {
+    fontFamily: fonts.regular_text,
+    fontSize: 18,
+  },
+  loginFormContainer: {
+    position: "absolute",
+    width: "100%",
+    top: "40%",
+    alignItems: "center",
+  },
+  loginForminput: {
+    width: "75%",
+    padding: 8,
+    borderWidth: 1,
+    borderColor: colors.borderLightColor,
+    borderRadius: 7,
+    marginBottom: 15,
+    textAlign: "center",
+    fontFamily: fonts.regular_text,
+  },
+  forgotPassContainer: {
+    position: "absolute",
+    width: "75%",
+    top: "55%",
+    marginTop: 3,
+    alignItems: "flex-end",
+  },
+  forgotPassHeading: {
+    color: colors.linkColor,
+    fontFamily: fonts.regular_text,
+  },
+  orDivContainer: {
+    position: "absolute",
+    top: "65%",
+    width: "100%",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  orDivText: {
+    fontFamily: fonts.regular_text,
+    fontSize: 15,
+    color: colors.borderLightColor,
+  },
+  signupHeadingContainer: {
+    position: "absolute",
+    top: "70%",
+    backgroundColor: colors.backgroundLight,
+    width: "100%",
+    height: "5%",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  signupHeading: {
+    fontFamily: fonts.regular_text,
+    fontSize: 18,
+  },
+  goToHomeButton: {
+    position: "absolute",
+    top: "90%",
+  },
+});
